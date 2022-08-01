c2a66c599e3caf4d40b0aa6adf4d913a2eb9e925
dmytro <blinnikov.dmytro@gmail.com>
2022-08-01 16:03:05 +0300

    Get JWT-token implemented

--- Cars_API/user_handler/urls.py
+++ Cars_API/user_handler/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
-    path('create/', RegisterView.as_view()),        # path('user/create', ),  # POST create new user
+    path('create/', RegisterView.as_view()),        # POST create new user
+    path('login/', MyObtainTokenPairView.as_view()),         # POST login user
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- Cars_API/user_handler/views.py
+++ Cars_API/user_handler/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, MyTokenObtainPairSerializer
 from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-    
+
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
