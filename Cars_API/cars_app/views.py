34b12cdd72df068f07193fca9c451d44fc863cf8
dmytro <blinnikov.dmytro@gmail.com>
2022-08-01 17:10:36 +0300

    Added permissions to API endpoints

--- Cars_API/cars_app/views.py
+++ Cars_API/cars_app/views.py
@@ -1,22 +1,27 @@
 from rest_framework import generics
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated
 
 
 class OnSaleCarsAPIView(generics.ListAPIView):
     queryset = Cars.objects.filter(is_on_sale=True)
     serializer_class = CarsSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 class AllCarsAPIView(generics.ListAPIView):
     queryset = Cars.objects.all()
     serializer_class = CarsSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 class ModelsAPIView(generics.ListAPIView):
     queryset = Model.objects.all()
     serializer_class = ModelSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 class BrandsAPIView(generics.ListAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    permission_classes = (IsAuthenticated, )
--- Cars_API/user_handler/views.py
+++ Cars_API/user_handler/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer, MyTokenObtainPairSerializer
+from rest_framework.permissions import AllowAny
 from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer, MyTokenObtainPairSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = (AllowAny, )
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+    permission_classes = (AllowAny, )
