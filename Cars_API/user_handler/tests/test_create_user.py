698b34404cd43a5594d149ce2f2e7eb30c1c2d72
dmytro <blinnikov.dmytro@gmail.com>
2022-08-14 12:58:25 +0300

    Code has been formatted using "black"

--- Cars_API/user_handler/tests/test_create_user.py
+++ Cars_API/user_handler/tests/test_create_user.py
@@ -3,32 +3,31 @@ from django.urls import reverse
 
 
 class TestCreateUserAPI(APITestCase):
-
     def setUp(self) -> None:
-        self.username = 'test_user'
-        self.email = 'test_user@gmail.com'
-        self.password = 'Pa$$word!@'
-        self.confirm_password = 'Pa$$word!@'
+        self.username = "test_user"
+        self.email = "test_user@gmail.com"
+        self.password = "Pa$$word!@"
+        self.confirm_password = "Pa$$word!@"
 
     def test_create_user(self):
         user_data = {
             "username": self.username,
             "email": self.email,
             "password": self.password,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data["username"], user_data.get("username"))
-        self.assertEqual(response.data['email'], user_data.get("email"))
+        self.assertEqual(response.data["email"], user_data.get("email"))
 
     def test_cannot_create_user_without_username(self):
         user_data = {
             "email": self.email,
             "password": self.password,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["username"], ["This field is required."])
 
@@ -36,9 +35,9 @@ class TestCreateUserAPI(APITestCase):
         user_data = {
             "username": self.username,
             "password": self.password,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["email"], ["This field is required."])
 
@@ -46,9 +45,9 @@ class TestCreateUserAPI(APITestCase):
         user_data = {
             "username": self.username,
             "email": self.email,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["password"], ["This field is required."])
 
@@ -56,9 +55,9 @@ class TestCreateUserAPI(APITestCase):
         user_data = {
             "username": self.username,
             "email": self.email,
-            "password": self.password
+            "password": self.password,
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["confirm_password"], ["This field is required."])
 
@@ -67,9 +66,9 @@ class TestCreateUserAPI(APITestCase):
             "username": self.username,
             "email": self.email,
             "password": self.password,
-            "confirm_password": "enother_pa$$word!@"
+            "confirm_password": "enother_pa$$word!@",
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["password"], ["Password fields didn't match."])
 
@@ -78,20 +77,20 @@ class TestCreateUserAPI(APITestCase):
             "username": self.username,
             "email": self.email,
             "password": self.password,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data["username"], user_data.get("username"))
-        self.assertEqual(response.data['email'], user_data.get("email"))
+        self.assertEqual(response.data["email"], user_data.get("email"))
 
         user_data_to_test = {
             "username": self.username,
             "email": "some.enother@gmail.com",
             "password": self.password,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data_to_test, format='json')
+        response = self.client.post(reverse("create"), user_data_to_test, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["username"], ["This field must be unique."])
 
@@ -100,21 +99,19 @@ class TestCreateUserAPI(APITestCase):
             "username": self.username,
             "email": self.email,
             "password": self.password,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data, format='json')
+        response = self.client.post(reverse("create"), user_data, format="json")
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data["username"], user_data.get("username"))
-        self.assertEqual(response.data['email'], user_data.get("email"))
+        self.assertEqual(response.data["email"], user_data.get("email"))
 
         user_data_to_test = {
             "username": "some_enother_username",
             "email": self.email,
             "password": self.password,
-            "confirm_password": self.confirm_password
+            "confirm_password": self.confirm_password,
         }
-        response = self.client.post(reverse('create'), user_data_to_test, format='json')
+        response = self.client.post(reverse("create"), user_data_to_test, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data["email"], ["This field must be unique."])
-
-
